faa18e63d0fa8e4310abd770fa2e267c8b1555ec
Woradorn Kamolpornwijit <chateau86@gmail.com>
2017-07-15 14:27:55 -0400

    Snake movement now mostly working.
    Signed-off-by: Woradorn Kamolpornwijit <chateau86@gmail.com>

--- SnakeGame/game.py
+++ SnakeGame/game.py
@@ -2,7 +2,9 @@
 #Woradorn K.
 import pprint
 import random
-
+import numpy as np
+dirMap = {0:(-1,0), 1:(0,1), 2:(1,0), 3:(0,-1)}
+decrementSnek = np.vectorize(lambda v: (v-1) if v>0 else v)
 class gameState:
     gameGrid = []
     snakeDir = 0 #hdg/90
@@ -10,35 +12,54 @@ class gameState:
     _gridSize = (0,0)
     _foodCount = 0
     _foodLoc = (0,0)
+    _headLoc = (0,0)
     _rng = random
     def __init__(self, w = 10, h = 10, seed = 0, start = (5,5)):
         self._gridSize = (w,h)
         self._rng.seed(seed)
-        for r in range(h):
-            self.gameGrid.append([])
-            for c in range(w):
-                self.gameGrid[r].append(0)
-        #pprint.pprint(self.gameGrid)
-        self.gameGrid[start[0]][start[1]] = 1
+        self.gameGrid = np.zeros(self._gridSize)
+        self.gameGrid[start] = 1
+        self._headLoc = (start)
         self._placeFood()
-        #print('---')
-        #pprint.pprint(self.gameGrid)
     def run(self, ctrlIn):
-        #void
-        return
+        #ctrlIn = {-1: turn left, 0: no turn, 1: turn right}
+        self.snakeDir += ctrlIn
+        self.snakeDir %= 4
+        dPos = dirMap[self.snakeDir]
+        newPos = tuple(np.mod(np.add(self._headLoc, dPos), self._gridSize))
+        lookAhead = self.gameGrid[newPos]
+        #print('lookahead at {} saw {}'.format(newPos,lookAhead))
+        assert(lookAhead <= 0), 'Ate self, died.'
+        #TODO: Death logic
+        if lookAhead == -1:
+            print('GOT FOOD')
+            self.score += 1
+            self._foodCount-= 1
+            self._placeFood()
+        else:
+            #now decrement snek
+            print('decrement snake')
+            self.gameGrid=decrementSnek(self.gameGrid)
+        print('add snake')
+        self.gameGrid[newPos] = self.score + 2
+        self._headLoc = newPos
+
+        self.printState()
         
     def look(self):
         #return what snake see
         return
     def printState(self):
+        print('hdg: {}'.format(self.snakeDir))
+        print('score: {}'.format(self.score))
         pprint.pprint(self.gameGrid)
         
     def _placeFood(self):
         if self._foodCount >= 1:
             return
         while self._foodCount < 1:
-            foodX = self._rng.randint(0,w)
-            foodY = self._rng.randint(0,h)
+            foodX = self._rng.randint(0,self._gridSize[0])
+            foodY = self._rng.randint(0,self._gridSize[1])
             if self.gameGrid[foodX][foodY] == 0:
                 self._foodLoc =(foodX,foodY)
                 self._foodCount += 1
--- SnakeGame/gameTest.py
+++ SnakeGame/gameTest.py
@@ -4,3 +4,12 @@ import pprint
 import game
 g = game.gameState()
 g.printState()
+g.score = 7
+while True:
+    for i in range(10):
+        g.run(0)
+        g.printState()
+    g.run(1)
+    g.printState()
+    g.run(-1)
+    g.printState()
