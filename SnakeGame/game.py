58e8905e8477861d69131d5a754d44d899759e97
Woradorn Kamolpornwijit <chateau86@gmail.com>
2017-07-15 14:14:41 -0400

    Snake movement now mostly working.
    Signed-off-by: Woradorn Kamolpornwijit <chateau86@gmail.com>

--- SnakeGame/game.py
+++ SnakeGame/game.py
@@ -2,7 +2,9 @@
 #Woradorn K.
 import pprint
 import random
-
+import numpy as np
+dirMap = {0:(-1,0), 1:(0,1), 2:(1,0), 3:(0,-1)}
+decrementSnek = np.vectorize(lambda v: (v-1) if v>0 else v)
 class gameState:
     gameGrid = []
     snakeDir = 0 #hdg/90
@@ -10,22 +12,40 @@ class gameState:
     _gridSize = (0,0)
     _foodCount = 0
     _foodLoc = (0,0)
+    _headLoc = (0,0)
     _rng = random
     def __init__(self, w = 10, h = 10, seed = 0, start = (5,5)):
         self._gridSize = (w,h)
         self._rng.seed(seed)
-        for r in range(h):
-            self.gameGrid.append([])
-            for c in range(w):
-                self.gameGrid[r].append(0)
+        self.gameGrid = np.zeros(self._gridSize)
+        #for r in range(h):
+        #    self.gameGrid.append([])
+        #    for c in range(w):
+        #        self.gameGrid[r].append(0)
         #pprint.pprint(self.gameGrid)
-        self.gameGrid[start[0]][start[1]] = 1
+        self.gameGrid[start] = 1
+        self._headLoc = (start)
         self._placeFood()
         #print('---')
         #pprint.pprint(self.gameGrid)
     def run(self, ctrlIn):
-        #void
-        return
+        #ctrlIn = {-1: turn left, 0: no turn, 1: turn right}
+        self.snakeDir += ctrlIn
+        self.snakeDir %= 4
+        print('hdg: {}'.format(self.snakeDir))
+        dPos = dirMap[self.snakeDir]
+        newPos = tuple(np.mod(np.add(self._headLoc, dPos), self._gridSize))
+        lookAhead = self.gameGrid[newPos]
+        print('lookahead at {} saw {}'.format(newPos,lookAhead))
+        assert(lookAhead <= 0), 'You ded'
+        self.gameGrid[newPos] = self.score + 2
+        self._headLoc = newPos
+        print('add snake')
+        #self.printState()
+        #now decrement snek
+        print('decrement snake')
+        self.gameGrid=decrementSnek(self.gameGrid)
+        self.printState()
         
     def look(self):
         #return what snake see
@@ -37,8 +57,8 @@ class gameState:
         if self._foodCount >= 1:
             return
         while self._foodCount < 1:
-            foodX = self._rng.randint(0,w)
-            foodY = self._rng.randint(0,h)
+            foodX = self._rng.randint(0,self._gridSize[0])
+            foodY = self._rng.randint(0,self._gridSize[1])
             if self.gameGrid[foodX][foodY] == 0:
                 self._foodLoc =(foodX,foodY)
                 self._foodCount += 1
