dc5a60997df863961020de944f72b7af81ae6003
ptrivedi31 <ptrivedi31@gatech.edu>
2017-07-16 20:50:17 -0700

    Added training and testing implementation. Also every best fitness genome is now saved as an .svg file

--- NEAT_income_test/NEAT_income_test.py
+++ NEAT_income_test/NEAT_income_test.py
@@ -1,4 +1,5 @@
 
+
 from __future__ import print_function
 import os
 import neat
@@ -9,25 +10,43 @@ import numpy as np
 #global numpy array
 X = np.array([])
 Y = np.array([])
+X_test = np.array([])
+Y_test = np.array([])
+generation = 0
 
 def initializeArrays():
     global X,Y
-    data = pd.read_csv("preprocessed_ID.csv", header=0)
+    data = pd.read_csv("training_preprocessed.csv", header=0)
     data.drop("Unnamed: 0", axis=1, inplace=True)
     Y = data.Prediction.values
     data.drop("Prediction", axis=1, inplace=True)
     X = data.values
+    data2 = pd.read_csv("testing_preprocessed.csv", header=0)
+    data2.drop("Unnamed: 0", axis=1, inplace=True)
+    Y_test = data2.Prediction.values
+    data2.drop("Prediction", axis=1, inplace=True)
+    X_test = data2.values
+    
     
 
 def eval_genomes(genomes, config):
+    global generation
     if len(X) == 0:
         initializeArrays()
+    fitnessList = []
     for genome_id, genome in genomes:
         genome.fitness = 0.0
         net = neat.nn.FeedForwardNetwork.create(genome, config)
         for xi, xo in zip(X,Y):
             output = net.activate(xi)
             genome.fitness -= (float(output - xo)) ** 2
+        fitnessList.append(genome.fitness)
+    maxIndex = fitnessList.index(max(fitnessList))
+    filename_EG = 'best' + str(generation)
+    winner_gid, winner_g = genomes[maxIndex]
+    visualize.draw_net(config, winner_g, False, filename=filename_EG)
+    generation += 1
+    
 
 
 def run(config_file):
@@ -49,19 +68,23 @@ def run(config_file):
     winner = p.run(eval_genomes, 300)
 
     # Display the winning genome.
-    #print('\nBest genome:\n{!s}'.format(winner))
+    print('\nBest genome:\n{!s}'.format(winner))
 
     # Show output of the most fit genome against training data.
-    print('\nOutput:')
     winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+    winner_fitness = 0.0
+    for xi, xo in zip(X_test, Y_test):
+        output = winner_net.activate(xi)
+        winner_fitness -= (float(output - xo)) ** 2
+    print("Winner Testing Fitness: ")
+    print(winner_fitness)    
 
-    #node_names = {-1:'A', -2: 'B', 0:'A XOR B'}
-    #visualize.draw_net(config, winner, True, node_names=node_names)
-    #visualize.plot_stats(stats, ylog=False, view=True)
-    #visualize.plot_species(stats, view=True)
+    visualize.draw_net(config, winner, False, filename='WINNER_NET')
+    visualize.plot_stats(stats, ylog=False)
+    visualize.plot_species(stats, view=True)
 
-    #p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
-    #p.run(eval_genomes, 10)
+    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
+    p.run(eval_genomes, 10)
 
 
 if __name__ == '__main__':
